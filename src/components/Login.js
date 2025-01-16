38c5047b33a06d3198767eacf030b2e4303f0c8e
Yonathan1 <nathan.gunawan93@gmail.com>
2025-01-16 10:06:32 +0700

    Integrate w/ Login API

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Input from "./form/Input";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { data, useNavigate, useOutletContext } from "react-router-dom";
 
 
 const Login = () => {
@@ -18,15 +18,39 @@ const Login = () => {
         event.preventDefault();
         console.log("email/pass ", email, password)
 
-        if (email === 'admin@example.com') {
-            setJwtToken("abc");
-            setAlertClassName("d-none");
-            setAlertMessage("");
-            navigate("/");
-        } else {
-            setAlertClassName("alert-danger");
-            setAlertMessage("Invalid credentials");
+        // build request payload
+        let payload = {
+            email: email,
+            password: password,
         }
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(payload),
+        }
+
+        fetch(`/authenticate`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if(data.error) {
+                    setAlertClassName("alert-danger");
+                    setAlertMessage(data.message);
+                }
+                else {
+                    setJwtToken(data.access_token);
+                    setAlertClassName("d-none");
+                    setAlertMessage("");
+                    navigate("/");
+                }
+            })
+            .catch(error => {
+                setAlertClassName("alert-danger");
+                setAlertMessage(error);
+            })
     }
 
     return(
