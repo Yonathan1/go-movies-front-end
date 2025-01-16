2041aea72442da97ec7a681b6712db840382d6ff
Yonathan1 <nathan.gunawan93@gmail.com>
2025-01-16 16:05:08 +0700

    Add refresh token and logout jwt integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Outlet, data, useNavigate } from 'react-router-dom';
 import Alert from './components/Alert';
 
 function App() {
@@ -10,9 +10,40 @@ function App() {
   const navigate = useNavigate();
 
   const logOut = () => {
-    setJwtToken("");
+    const requestOptions = {
+      method: "GET",
+      credentials: "include",
+    }
+
+    fetch("/logout", requestOptions)
+      .catch(error => {
+        console.log("error logging out", error);
+      })
+      .finally(() => {
+        setJwtToken("");
+      })
     navigate("/login");
   }
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      const requestOptions = {
+        method: "GET",
+        credentials: "include",
+      }
+
+      fetch("/refresh", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.access_token) {
+            setJwtToken(data.access_token);
+          }
+        })
+        .catch(error => {
+          console.log("user is not logged in", error);
+        })
+    }
+  }, [jwtToken])
 
   return (
     <div className="container">
