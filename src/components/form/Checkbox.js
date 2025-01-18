ba17b32b29c8df600ec72797521573110f834264
Yonathan1 <nathan.gunawan93@gmail.com>
2025-01-18 10:10:19 +0700

    Add textarea, select & checkbox component

---
+++ src/components/form/Checkbox.js
@@ -0,0 +1,19 @@
+const Checkbox = (props) => {
+    return (
+        <div className="form-check">
+            <input
+                className="form-check-input"
+                id={props.name}
+                name={props.name}
+                value={props.value}
+                onChange={props.onChange}
+                checked={props.checked}
+            />
+            <label className="form-check-label" htmlFor={props.name}>
+                {props.title}
+            </label>
+        </div>
+    )
+}
+
+export default Checkbox;
